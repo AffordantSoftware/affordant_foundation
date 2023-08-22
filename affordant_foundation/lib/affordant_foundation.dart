e946ce994c67c2a22c36b0ac9c460850c8b3471c
Pierre2tm <Pieree2tm@gmail.com>
2023-08-22 14:57:42 +0900

    affordant foundation

---
+++ affordant_foundation/lib/affordant_foundation.dart
@@ -0,0 +1,6 @@
+export 'package:affordant_core/affordant_core.dart';
+export 'package:affordant_bloc/affordant_bloc.dart';
+export 'package:affordant_flutter_bloc/affordant_flutter_bloc.dart';
+export 'package:affordant_auth/affordant_auth.dart';
+export 'package:affordant_form/affordant_form.dart';
+export 'package:affordant_navigation/affordant_navigation.dart';
