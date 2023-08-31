17cbcf89ad8665b00dabd16a4912e3f4be07f04f
Pierre2tm <Pieree2tm@gmail.com>
2023-08-31 14:36:37 +0900

    replace bind by bind2 and introduce bindAndConsume

--- affordant_flutter_view_model/lib/src/bind.dart
+++ affordant_flutter_view_model/lib/src/bind.dart
@@ -4,39 +4,54 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:affordant_view_model/affordant_view_model.dart';
 
-class Bind<T> extends StatelessWidget {
+class Bind<T extends ViewModel> extends StatelessWidget {
   const Bind({
-    required this.viewModel,
-    required this.builder,
-    super.key,
-  });
-
-  final ViewModel<T> viewModel;
-  final Widget Function(BuildContext context, T value) builder;
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => viewModel,
-      child: BlocBuilder<ViewModel<T>, T>(
-        builder: builder,
-      ),
-    );
-  }
-}
-
-class Bind2<T extends ViewModel> extends StatelessWidget {
-  const Bind2({
     required this.viewModel,
     required this.child,
     super.key,
   });
+
+  static BindAndConsume consume<T extends ViewModel<S>, S>({
+    required T viewModel,
+    required final Widget Function(BuildContext context, S state) builder,
+    Key? key,
+  }) =>
+      BindAndConsume<T, S>(
+        viewModel: viewModel,
+        builder: builder,
+        key: key,
+      );
 
   final T viewModel;
   final Widget child;
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider<T>(create: (context) => viewModel, child: child);
+    return BlocProvider<T>(
+      create: (context) => viewModel,
+      child: child,
+    );
+  }
+}
+
+class BindAndConsume<T extends ViewModel<S>, S> extends StatelessWidget {
+  const BindAndConsume({
+    required this.viewModel,
+    required this.builder,
+    super.key,
+  });
+
+  final T viewModel;
+  final Widget Function(BuildContext context, S state) builder;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider<T>(
+      create: (context) => viewModel,
+      child: BlocBuilder(
+        bloc: viewModel,
+        builder: builder,
+      ),
+    );
   }
 }
