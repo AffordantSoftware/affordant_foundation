befacc3908865dff660cf97b7c032e3e968db132
Pierre2tm <Pieree2tm@gmail.com>
2023-09-12 11:34:53 +0700

    remove dependency injection

--- affordant_core/lib/affordant_core.dart
+++ affordant_core/lib/affordant_core.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/widgets.dart';
-import 'package:get_it/get_it.dart';
+
+export 'package:get_it/get_it.dart' show Disposable;
 
 mixin Displayable {
   String display(BuildContext context);
 }
 
-mixin DependencyInjection {
-  GetIt get di => GetIt.I;
+base class DisplayableError with Displayable {
+  DisplayableError(this.error, this.stackTrace);
+
+  final dynamic error;
+  final StackTrace stackTrace;
+
+  @override
+  String display(BuildContext context) {
+    return "error toString";
+  }
 }
