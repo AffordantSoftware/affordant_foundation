1790989b993f82c6e492cbb94729f7a87ac5f37c
Pierre2tm <Pieree2tm@gmail.com>
2023-09-12 11:55:07 +0700

    remove flutter_view_model

--- affordant_foundation/lib/affordant_foundation.dart
+++ affordant_foundation/lib/affordant_foundation.dart
@@ -1,6 +1,5 @@
 export 'package:affordant_core/affordant_core.dart';
 export 'package:affordant_view_model/affordant_view_model.dart';
-export 'package:affordant_flutter_view_model/affordant_flutter_view_model.dart';
 export 'package:affordant_auth/affordant_auth.dart';
 export 'package:affordant_form/affordant_form.dart';
 export 'package:affordant_navigation/affordant_navigation.dart';
