81b251cd57f81add1a31f048e91a0594fbee42ed
Pierre2tm <Pieree2tm@gmail.com>
2023-09-12 12:11:05 +0700

    re-export provider

--- affordant_view_model/lib/affordant_view_model.dart
+++ affordant_view_model/lib/affordant_view_model.dart
@@ -1,2 +1,5 @@
 export 'src/view_model.dart';
 export 'src/bind.dart';
+
+export 'package:flutter_bloc/flutter_bloc.dart'
+    show SelectContext, ReadContext, WatchContext, ProviderNotFoundException;
