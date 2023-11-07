ed1a15e3c69be4de9658c8f888773be6cac36ba1
Pierre2tm <Pieree2tm@gmail.com>
2023-11-07 12:47:21 +0700

    add onboarding to fundation

--- affordant_foundation/lib/affordant_foundation.dart
+++ affordant_foundation/lib/affordant_foundation.dart
@@ -3,3 +3,4 @@ export 'package:affordant_view_model/affordant_view_model.dart';
 export 'package:affordant_auth/affordant_auth.dart';
 export 'package:affordant_form/affordant_form.dart';
 export 'package:affordant_navigation/affordant_navigation.dart';
+export 'package:affordant_onboarding/affordant_onboarding.dart';
