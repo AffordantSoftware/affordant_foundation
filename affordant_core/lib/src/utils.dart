8f1d38f88c197877460559d4d62e8120a86d6b3a
Pierre2tm <Pieree2tm@gmail.com>
2023-11-22 17:36:05 +0700

    handle network fail exception

--- affordant_core/lib/src/utils.dart
+++ affordant_core/lib/src/utils.dart
@@ -1,9 +1,13 @@
+import 'dart:io';
+
 import 'package:affordant_core/affordant_core.dart';
 
 /// Execute a server call, catch any error and convert them to `UnspecifiedServerException`
 T safeServerCall<T>(T Function() run) {
   try {
     return run();
+  } on SocketException catch (e, s) {
+    throw NetworkUnavailableException(e, s);
   } catch (e, s) {
     throw UnspecifiedServerException(e, s);
   }
