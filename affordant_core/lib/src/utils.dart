21df11a7f7caf180457a36b522d591ac4173095e
Pierre2tm <Pieree2tm@gmail.com>
2023-11-22 17:51:58 +0700

    core: fix safeCall not catching error

--- affordant_core/lib/src/utils.dart
+++ affordant_core/lib/src/utils.dart
@@ -1,11 +1,12 @@
+import 'dart:async';
 import 'dart:io';
 
 import 'package:affordant_core/affordant_core.dart';
 
 /// Execute a server call, catch any error and convert them to `UnspecifiedServerException`
-T safeServerCall<T>(T Function() run) {
+Future<T> safeServerCall<T>(FutureOr<T> Function() run) async {
   try {
-    return run();
+    return await run();
   } catch (e, s) {
     if (e is DisplayableException) {
       rethrow;
