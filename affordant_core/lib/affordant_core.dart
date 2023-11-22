d6c1615d7fb6636943373810763f86fd07f865f1
Pierre2tm <Pieree2tm@gmail.com>
2023-11-22 16:24:15 +0700

    update displayable exception

--- affordant_core/lib/affordant_core.dart
+++ affordant_core/lib/affordant_core.dart
@@ -6,8 +6,25 @@ mixin Displayable {
   String display(BuildContext context);
 }
 
-abstract class DisplayableException with Displayable {
-  const DisplayableException(this.stackTrace);
+/// How to write a good error message:
+/// Title: Say what happened
+/// Message:
+/// 1. Provides reassurance
+/// 2. Say why its happened
+/// 3. Help user fix issue
+/// 4. Give a way out
+///
+/// Example:
+/// Unable to save your changes
+/// Your changes are saved locally, but we are unable to connect to your account because your device not connected to internet.
+/// Please re-connect and try again. If the problem persist, contact [Customer Care](http://customercare.com)
+abstract class DisplayableException {
+  const DisplayableException(
+    this.stackTrace,
+  );
 
   final StackTrace stackTrace;
+
+  String localizedTitle(BuildContext context);
+  String localizedMessage(BuildContext context);
 }
