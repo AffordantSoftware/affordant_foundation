040a76249d48ef19391aba085bc2293f8d48e690
Pierre2tm <Pieree2tm@gmail.com>
2023-11-24 11:41:01 +0700

    improved l1àn

--- affordant_auth/lib/src/l10n/affordant_auth_localizations_fr.dart
+++ affordant_auth/lib/src/l10n/affordant_auth_localizations_fr.dart
@@ -8,11 +8,11 @@ class AffordantAuthLocalizationsFr extends AffordantAuthLocalizations {
   String get error_registration_account_already_exists_title => 'Adresse email indisponible.';
 
   @override
-  String get error_registration_account_already_exists_message => 'Un compte avec cette address email exist déjà. Essayez de vous connecter.';
+  String get error_registration_account_already_exists_message => 'Un compte associé à cette address email existe déjà. Essayez de vous connecter.';
 
   @override
   String get error_sign_in_invalid_credentials_title => 'Identifiants invalides.';
 
   @override
-  String get error_sign_in_invalid_credentials_message => 'Les identifiants que vous avez fournis ne sont invalides.';
+  String get error_sign_in_invalid_credentials_message => 'Les identifiants fournis sont invalides.';
 }
