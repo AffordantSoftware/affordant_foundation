b5b374dee9ad8a832db35a758fe27b62f3314b0f
Pierre2tm <Pieree2tm@gmail.com>
2023-12-13 15:52:51 +0700

    foundation: delete package

--- affordant_foundation/lib/affordant_foundation.dart
+++
@@ -1,6 +0,0 @@
-export 'package:affordant_core/affordant_core.dart';
-export 'package:affordant_view_model/affordant_view_model.dart';
-export 'package:affordant_auth/affordant_auth.dart';
-export 'package:affordant_form/affordant_form.dart';
-export 'package:affordant_navigation/affordant_navigation.dart';
-export 'package:affordant_onboarding/affordant_onboarding.dart';
