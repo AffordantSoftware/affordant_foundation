81cc9f2a9b699dc27ff54f13d95e54abe47f1ec9
Pierre2tm <Pieree2tm@gmail.com>
2023-12-13 15:59:58 +0700

    core: doesn't export get_it anymore

--- affordant_core/lib/affordant_core.dart
+++ affordant_core/lib/affordant_core.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/widgets.dart';
 
-export 'package:get_it/get_it.dart' show Disposable;
-
 export 'src/l10n/affordant_core_localizations.dart';
 export 'src/exceptions.dart';
 export 'src/utils.dart';
