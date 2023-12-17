4df4d1d4d1ab65eb55e214acb422c82553ffe7c3
Pierre2tm <Pieree2tm@gmail.com>
2023-12-17 10:56:18 +0700

    core: great simplification and abandon Result

--- affordant_core/lib/affordant_core.dart
+++ affordant_core/lib/affordant_core.dart
@@ -1,14 +1,4 @@
-import 'package:flutter/widgets.dart';
-
-export 'src/l10n/affordant_core_localizations.dart';
-export 'src/utils/network_call.dart';
-export 'src/utils/utils.dart';
+// export 'src/l10n/affordant_core_localizations.dart';
+// export 'src/utils/utils.dart';
 export 'src/nullable_extension.dart';
-export 'src/result.dart';
 export 'src/error.dart';
-export 'src/repository.dart';
-
-@Deprecated("Use specialized class instead. eg. [DisplayableError]")
-mixin Displayable {
-  String display(BuildContext context);
-}
--- affordant_core/lib/src/error.dart
+++ affordant_core/lib/src/error.dart
@@ -1,8 +1,4 @@
-import 'dart:async';
-
 import 'package:flutter/widgets.dart';
-
-import 'result.dart';
 
 /// How to write a good error message:
 /// Title: Say what happened
@@ -16,72 +12,7 @@ import 'result.dart';
 /// Unable to save your changes
 /// Your changes are saved locally, but we are unable to connect to your account because your device not connected to internet.
 /// Please re-connect and try again. If the problem persist, contact [Customer Care](http://customercare.com)
-base mixin DisplayableError on Error {
+mixin DisplayableError {
   String localizedTitle(BuildContext context);
   String localizedMessage(BuildContext context);
 }
-
-extension FutureResultErrorExtension<T, E extends Error>
-    on Future<Result<T, E>> {
-  /// If add context to Error if Result is Err
-  Future<Result<T, E>> withContext(String context) => then(
-        (r) => r.withContext(context),
-      );
-}
-
-extension FutureOrResultErrorExtension<T, E extends Error>
-    on FutureOr<Result<T, E>> {
-  /// If add context to Error if Result is Err
-  FutureOr<Result<T, E>> withContext(String context) => switch (this) {
-        final Future f => f.then((r) => r.withContext(context)),
-        _ => this.withContext(context),
-      };
-}
-
-extension ResultErrExtension<T, E extends Error> on Result<T, E> {
-  /// If add context to Error if Result is Err
-  Result<T, E> withContext(String context) {
-    if (this case Err(:final err)) {
-      err.withContext(context);
-    }
-    return this;
-  }
-}
-
-base class Error {
-  Error(this.error, [StackTrace? stackTrace, String? context])
-      : _stackTrace = error is Error ? null : stackTrace ?? StackTrace.current,
-        _contexts = [if (context != null) context];
-
-  /// Enrich error with context
-  void withContext(String context) {
-    _contexts.add(context);
-  }
-
-  final dynamic error;
-
-  final StackTrace? _stackTrace;
-
-  final List<String> _contexts;
-
-  StackTrace? get stackTrace => error is Error ? error.stackTrace : _stackTrace;
-
-  Iterable<String> get contexts => [
-        if (error is Error) ...error.contexts,
-        ..._contexts,
-      ];
-}
-
-// final class UnknownError extends Error with DisplayableError {
-//   UnknownError({super.error, super.stackTrace, super.context});
-
-//   @override
-//   String localizedMessage(BuildContext context) {
-//     return "unknown";
-//   }
-
-//   @override
-//   String localizedTitle(BuildContext context) {
-//     return "unknown";
-//   }
-// }
